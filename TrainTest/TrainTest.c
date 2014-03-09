68e451f4e150c2ffa4dab54b1680f0dbac041ec8
Luke <luke.wallin@gmail.com>
2014-03-09 21:43:47 +0000

    progress on the main transmission state machine

--- TrainTest/TrainTest.c
+++ TrainTest/TrainTest.c
@@ -12,6 +12,7 @@
 #include <util/delay.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
+#include <stdbool.h>
 
 #include "TrainTest.h"
 
@@ -25,17 +26,48 @@
 //volatile int64_t pwm;
 //volatile int8_t dirstate;
 
+#define PACKET_BUFFER_SIZE (8)
+#define MAX_DATA_BYTES (4)
+
+#define PREAMBLE_LENGTH (16)
+
+//the information required for a packet.  From this a whole real packet can be generated
+typedef struct{
+	int8_t address;
+	int8_t dataBytes;//just the actual data bytes, we will work out the error detection bytes at transmission time
+	int8_t data[MAX_DATA_BYTES];
+	//note that an error detection byte is a different type of data byte
+}packetData_t;
 
 
+//buffer to hold packets info to be sent
+packetData_t packetBuffer[PACKET_BUFFER_SIZE];
+//volatile int16_t packetBufferPosition;
 
+//where in the packet are we currently?
 enum transmitStates{
-	FORWARDS,
-	STOP1,
-	BACKWARDS,
-	STOP2
+	PREAMBLE,
+	PACKET_START_BIT,
+	ADDRESS,
+	DATA_START_BIT,
+	DATA,
+	ERROR_START_BIT,
+	ERROR_DETECTION,//this and data will be interleaved if more than one data byte
+	END_BIT
 };
 
+//which state are we currently in?
 volatile int8_t transmitState;
+
+//the packet we are currently transmitting
+volatile int8_t transmittingPacket;
+
+//which bit of the preamble/address/data/errordetect are we transmitting?
+volatile int8_t transmittingBit;
+//volatile int8_t addressBit;
+//which data byte are we transmitting?
+volatile int8_t transmittingDataByte;
+
 
 //The different states which are cycled through when transmitting data
 //a 1 is transmitted by being high during ONE_HIGH and then low during ONE_LOW
@@ -43,7 +75,7 @@ volatile int8_t transmitState;
 //0 is tramsitted likewise, but high and low take twice as long
 //(spec calls for >100us, so I'm using 116us because this is easy)
 //See S91-2004-07 "A: Technique For Encoding Bits"
-enum bitState{
+enum bitStates{
 	ONE_HIGH,
 	ONE_LOW,
 	ZERO_HIGH1,
@@ -52,12 +84,17 @@ enum bitState{
 	ZERO_LOW2
 };
 
+volatile int8_t bitState;
+//
+////given position in packetBuffer, get a pointer to that packet
+//packetData_t *getPacket(int8_t packetId)
+//{
+	//return 
+//}
+
 int main(void)
 {
-	
-	pwm=0;
-	dirstate = FORWARDS;
-	
+		
 	//Set timer0 to CTC mode (clears timer on compare match)
 	//TCCR0A,WGM02;//=2;
 	Clrb(TCCR0A,WGM00);
@@ -71,15 +108,15 @@ int main(void)
 	//enable interrupt on compare match on timer0
 	//Setb(TIFR0,OCF0A);
 	
-	////set counter's clock to be systemclock/1024
-	//Setb(TCCR0B,CS00);
-	//Clrb(TCCR0B,CS01);
-	//Setb(TCCR0B,CS02);
+	//set counter's clock to be systemclock/1024
+	Setb(TCCR0B,CS00);
+	Clrb(TCCR0B,CS01);
+	Setb(TCCR0B,CS02);
 	//
 	//what is the compare value for the timer?
 	OCR0A=254;
 	#else
-	//set counter's clock to be systemclock/8
+	//set counter's clock to be systemclock/8 (so clock to timer will be 1MHz)
 	Clrb(TCCR0B,CS00);
 	Setb(TCCR0B,CS01);
 	Clrb(TCCR0B,CS02);
@@ -95,9 +132,9 @@ int main(void)
 	////set led pin to output
 	//setb(DCC_direction,DCC_pin);
 	//
-	//turn led pin on
-	Setb(DCC_PORT,DCC_PIN0);
-	Setb(DCC_PORT,DCC_PIN1);
+	//set DCC pins to output
+	Setb(DCC_DIRECTION,DCC_PIN0);
+	Setb(DCC_DIRECTION,DCC_PIN1);
 	//
 	//while(1)
 	//{
@@ -115,33 +152,192 @@ int main(void)
 	}
 }
 
+inline packetData_t *currentPacket()
+{
+	return	&(packetBuffer[transmittingPacket]);
+}
+//we've reached the end of transmitting a bit, need to set up the state to transmit the next bit
+int8_t determineNextBit()
+{
+	switch(transmitState)
+	{
+		case PREAMBLE:
+			transmittingBit++;
+			if(transmittingBit > PREAMBLE_LENGTH)
+			{
+				//finished transmitting the preamble!
+				transmitState = PACKET_START_BIT;
+				transmittingBit=0;
+				return ZERO_HIGH1;
+			}else{
+				//still mroe preamble bits to go
+				return ONE_HIGH;
+			}
+			break;
+		case PACKET_START_BIT:
+			//finished transmitting the packet start bit, start transmitting address
+			transmittingBit=0;
+			transmitState = ADDRESS;
+			packetData_t *d = currentPacket();
+			if(d->address & (1 << 8))
+			{
+				//MSB of address is 1
+				return ONE_HIGH;
+			}
+			else
+			{
+				return ZERO_HIGH1;
+			}
+			break;
+		case ADDRESS:
+			transmittingBit++;
+			
+			if(transmittingBit >= 8){
+				if(currentPacket()->dataBytes > 0)
+				{
+					//there is data to transmit
+					transmitState=DATA_START_BIT;
+					return ZERO_HIGH1;
+				}else{
+					//no data to transmit
+					transmitState=END_BIT;
+					return ONE_HIGH;
+				}
+				
+				
+			}else{
+				//still address data to transmit
+				if(d->address & (1 << (8-transmittingBit)))
+				{
+					//jnext MSB of address is 1
+					return ONE_HIGH;
+				}
+				else
+				{
+					return ZERO_HIGH1;
+				}
+			}
+			break;
+		case DATA_START_BIT:
+			transmittingBit=0;
+			transmitState = DATA;
+			if(currentPacket()->data[transmittingDataByte] & (1 << 8))
+			{
+				//MSB of data byte is 1
+				return ONE_HIGH;
+			}
+			else
+			{
+				return ZERO_HIGH1;
+			}
+			break;
+		case DATA:
+			transmittingBit++;
+			
+			if(transmittingBit >= 8){
+				//reached end of this data byte
+				transmitState = ERROR_START_BIT;
+				return ZERO_HIGH1;
+			}else{
+				//still data bits to transmit
+				if(d->data[transmittingDataByte] & (1 << (8-transmittingBit)))
+				{
+					//jnext MSB of address is 1
+					return ONE_HIGH;
+				}
+				else
+				{
+					return ZERO_HIGH1;
+				}
+			}
+			break;
+		case ERROR_START_BIT:
+			transmittingBit=0;
+			transmitState = ERROR_DETECTION;
+			//error detection byte is xor of the data byte and address
+			if((currentPacket()->data[transmittingDataByte] ^ currentPacket()->address) & (1 << 8))
+			{
+				//MSB of error detection byte is 1
+				return ONE_HIGH;
+			}
+			else
+			{
+				return ZERO_HIGH1;
+			}
+			break;
+		case ERROR_DETECTION:
+			transmittingBit++;
+		
+			if(transmittingBit >= 8){
+				//reached end of this error detection byte
+				transmitState = END_BIT;
+				return ONE_HIGH;
+			}else{
+				//still error detection bits to transmit
+				if((currentPacket()->data[transmittingDataByte] ^ currentPacket()->address) & (1 << (8-transmittingBit)))
+				{
+					//jnext MSB of errordetect is 1
+					return ONE_HIGH;
+				}
+				else
+				{
+					return ZERO_HIGH1;
+				}
+			}
+			break;
+		case END_BIT:
+		default:
+			//TODO next data packet!!
+			return ONE_HIGH;
+			break;
+	}
+	
+}
+
 /************************************************************************/
 /* Interrupt which is run every 58us                                    */
 /************************************************************************/
 ISR(TIMER0_COMPA_vect)
 {
-	pwm++;
-	if(pwm %6000==0)
+
+	//output the right bit
+	switch(bitState)
 	{
-		switch(dirstate)
-		{
-			
-			case FORWARDS:
+		case ONE_HIGH:
+		case ZERO_HIGH1:
+		case ZERO_HIGH2:
+
 			Setb(DCC_PORT,DCC_PIN1);
 			Clrb(DCC_PORT,DCC_PIN0);
-			break;
-			case BACKWARDS:
+		case ONE_LOW:
+		case ZERO_LOW1:
+		case ZERO_LOW2:
 			Setb(DCC_PORT,DCC_PIN0);
 			Clrb(DCC_PORT,DCC_PIN1);
-			break;
-			default:
-			Clrb(DCC_PORT,DCC_PIN0);
-			Clrb(DCC_PORT,DCC_PIN1);
-			break;
-		}
-		
-		//DCC_PORT ^= (1 << DCC_PIN1); // Toggle the LED
-		dirstate++;
-		dirstate%=4;
+		break;
 	}
+	//proceed to output the rest of this bit, or work out what the next bit is
+	switch(bitState)
+	{
+		case ONE_HIGH:
+			bitState = ONE_LOW;
+			break;
+		case ZERO_HIGH1:
+			bitState=ZERO_HIGH2;
+			break;
+		case ZERO_HIGH2:
+			bitState = ZERO_LOW1;
+			break;
+		case ZERO_LOW1:
+			bitState = ZERO_LOW2;
+			break;
+			
+		case ONE_LOW:
+		case ZERO_LOW2:
+		default:
+			//need the next bit!
+			bitState = determineNextBit();
+			break;
+	}
+
 }
